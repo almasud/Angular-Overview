b4334a148d78402b96ff3769868288b00393b1eb
almasud <dev.almasud@gmail.com>
2022-08-17 18:41:21 +0600

    Component with inline template and inside CSS.

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-test', // Custom HTML tag selector
+  // selector: '.app-test', // HTML class name selector
+  // selector: '[app-test]', // HTML custom attribute selector
+  // templateUrl: './test.component.html', // HTML template URL
+  template: `<div>
+                Inline template
+              </div>`,  // Inline template
+  // styleUrls: ['./test.component.css']  // CSS file URL
+  styles: [`
+      div {
+        color: red;
+      }
+  `],  // Inside CSS
 })
 export class TestComponent implements OnInit {
 
