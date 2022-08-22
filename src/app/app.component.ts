8bf9b4c50457bb65be6c77e4154e073730777c76
almasud <dev.almasud@gmail.com>
2022-08-22 12:42:08 +0600

    Component interaction.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Overview';
+  public name = "Almasud";
+  public message = "";
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test', // Custom HTML tag selector
   // selector: '.app-test', // HTML class name selector
   // selector: '[app-test]', // HTML custom attribute selector
   // templateUrl: './test.component.html', // HTML template URL
-  template: `<div>
-                Inline template
-              </div>`,  // Inline template
+  template: `
+      <h2>Hello {{name}}</h2>
+      <button (click)="fireEvent()">Send Event to Parent</button>
+  `,  // Inline template
   // styleUrls: ['./test.component.css']  // CSS file URL
   styles: [`
       div {
@@ -16,10 +17,16 @@ import { Component, OnInit } from '@angular/core';
   `],  // Inside CSS
 })
 export class TestComponent implements OnInit {
+  @Input('parentName') public name: any;
+  @Output() public childEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fireEvent() {
+    this.childEvent.emit("Hey, Angular");
   }
 
 }
