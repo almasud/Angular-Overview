b5be65f5934b447a1ce13358115da6f7b21902a6
almasud <dev.almasud@gmail.com>
2022-08-22 11:58:05 +0600

    ngSwitch and ngFor

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,8 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <div *ngIf="true">ngIF</div>
+    <div *ngIf="isNameVisible">ngIF</div>
     <div *ngIf="isNameVisible; then nameBlock else nameHiddenBlock"></div>
+
+    <div *ngIf="isNameVisible">ngSwitch</div>
+    <div [ngSwitch]="color">
+      <h2 *ngSwitchCase="'red'">You picked red color.</h2>
+      <h2 *ngSwitchCase="'green'">You picked green color.</h2>
+      <h2 *ngSwitchCase="'blue'">You picked blue color.</h2>
+      <h2 *ngSwitchDefault>Picked agian.</h2>
+    </div>
+
+    <div *ngIf="isNameVisible">ngFor</div>
+    <h2 style="display: Inline;" *ngFor="let color of colors; index as i; last as l; odd as o">
+      [{{i}}] <span [style.color]="o ? 'orange' : 'orange'">{{color}} <span *ngIf="!l">, </span></span>
+    </h2>
 
     <ng-template #nameBlock>
       <h2>{{getGreetings()}}</h2>
@@ -21,6 +34,8 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   private name = "Almasud";
   public isNameVisible = true;
+  public color = "red";
+  public colors = ["red", "green", "blue"];
 
   constructor() { }
 
