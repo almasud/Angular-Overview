9c1f47e39a97ce4c8fddc15d0897c69bd9c4b32d
almasud <dev.almasud@gmail.com>
2022-08-23 15:48:07 +0600

    mparamMap observable.

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { AppConstant } from '../util/app_constants';
 
 @Component({
   selector: 'app-department-details',
   template: `
     <h3>Youe selected depatment with id: {{departmentId}}</h3>
+    <button (click)="goPrevious()">Previous</button>
+    <button (click)="goNext()">Next</button>
   `,
   styles: [
   ]
 })
 export class DepartmentDetailsComponent implements OnInit {
-  public departmentId: any
-  constructor(private activatedRoute: ActivatedRoute) { }
+  public departmentId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.departmentId = this.activatedRoute.snapshot.paramMap.get('id');
+    // When we try to go back or go forward in the same component angular reuse this isntead of loading as new
+    // this.departmentId = parseInt(this.activatedRoute.snapshot.paramMap.get('id')!);
+
+    // To overcome this we can use observable instead of snapshot to get the new route 
+    // Id while navigating in the same page.
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.departmentId = parseInt(paramMap.get('id')!);
+    });
+
+  }
+
+  goPrevious() {
+    this.router.navigate([AppConstant.ROUTE_DEPARTMENTS, this.departmentId - 1]);
+  }
+
+  goNext() {
+    this.router.navigate([AppConstant.ROUTE_DEPARTMENTS, this.departmentId + 1]);
   }
 
 }
