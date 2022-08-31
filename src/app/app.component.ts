02c4bd153a6ac263098b9707792e0827104c490e
almasud <dev.almasud@gmail.com>
2022-08-31 11:42:05 +0600

    Data binding to a model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+  userModel = new User(
+    "Almasud", 
+    "dev.almasud@gmail.com", 
+    "015000000", 
+    this.topics[0], 
+    "morning", 
+    true
+  );
 }
